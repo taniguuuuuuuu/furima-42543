679813d7d0523f647598ef072122a7e4be944bc7
taniguuuuuuuu <miiiisaaaakiiii.920616@gmail.com>
2025-09-26 18:15:32 +0900

    ユーザー管理機能修正①

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,19 +5,24 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # ニックネーム
-  validates :nickname, presence: true
+  validates :nickname, presence: { message: "can't be blank" }
 
   # お名前(全角)
-  validates :last_name,  presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字で入力してください' }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字で入力してください' }
+  validates :last_name,  presence: { message: "can't be blank" },
+                         format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
+  validates :first_name, presence: { message: "can't be blank" },
+                         format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
 
   # お名前カナ(全角)
-  validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' }
+  validates :last_name_kana,  presence: { message: "can't be blank" },
+                              format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters' }
+  validates :first_name_kana, presence: { message: "can't be blank" },
+                              format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters' }
 
   # 生年月日
-  validates :birthday, presence: true
+  validates :birthday, presence: { message: "can't be blank" }
 
   # パスワード
-  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'は半角英数字を両方含む必要があります' }
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/,
+                                 message: 'is invalid. Include both letters and numbers' }
 end
