d96d6a4038a2a543cb4a0e0be4de3ed7551ab85e
taniguuuuuuuu <miiiisaaaakiiii.920616@gmail.com>
2025-10-03 00:49:41 +0900

    商品出品機能修正③

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_29_140924) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_02_154102) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -45,14 +45,14 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_29_140924) do
   end
 
   create_table "items", charset: "utf8mb3", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.integer "status_id"
-    t.integer "detail_id"
-    t.integer "delivery_fee_id"
-    t.integer "prefecture_id"
-    t.integer "shipping_days_id"
-    t.integer "price"
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "status_id", null: false
+    t.integer "detail_id", null: false
+    t.integer "delivery_fee_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "shipping_days_id", null: false
+    t.integer "price", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
