262a87310a66e950151e124497189cf14456057d
taniguuuuuuuu <miiiisaaaakiiii.920616@gmail.com>
2025-10-23 02:07:47 +0900

    商品購入機能修正②

--- db/migrate/20251020130437_create_order_forms.rb
+++
@@ -1,8 +0,0 @@
-class CreateOrderForms < ActiveRecord::Migration[7.1]
-  def change
-    create_table :order_forms do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_10_20_130437) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_19_140913) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -57,11 +57,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_20_130437) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_items_on_user_id"
-  end
-
-  create_table "order_forms", charset: "utf8mb3", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "purchases", charset: "utf8mb3", force: :cascade do |t|
