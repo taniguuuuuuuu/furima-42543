b875ad7d589c2f67614e525baed5257ef9946385
taniguuuuuuuu <miiiisaaaakiiii.920616@gmail.com>
2025-10-23 23:43:34 +0900

    最終報告

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,7 @@ class OrdersController < ApplicationController
   def create
     @order_form = OrderForm.new(order_params)
     if @order_form.valid?
+      pay_item
       @order_form.save
       redirect_to root_path
     else
