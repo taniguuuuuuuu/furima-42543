99ff5923175ef02ee0df6a170c7e3aa919377d7e
taniguuuuuuuu <miiiisaaaakiiii.920616@gmail.com>
2025-10-24 17:34:27 +0900

    最終報告修正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,6 +1,3 @@
-// app/javascript/price.js
-import "payjp"; // ← PayJPスクリプトを読み込む
-
 
 const price = () => {
   const priceInput = document.getElementById("item-price");
@@ -16,62 +13,13 @@ const price = () => {
       taxDom.innerHTML = "";
       profitDom.innerHTML = "";
     } else {
-      const tax = Math.floor(inputValue * 0.1);   // 販売手数料
-      const profit = Math.floor(inputValue - tax); // 販売利益
+      const tax = Math.floor(inputValue * 0.1);
+      const profit = Math.floor(inputValue - tax);
       taxDom.innerHTML = tax;
       profitDom.innerHTML = profit;
     }
   });
 };
 
-
-const pay = () => {
-  const payjpPublicKey = document
-    .querySelector('meta[name="payjp-public-key"]')
-    ?.getAttribute("content");
-
-  if (!payjpPublicKey) {
-    console.error("PAYJP_PUBLIC_KEY が読み込めませんでした。");
-    return;
-  }
-
-  const payjp = Payjp(payjpPublicKey);
-  const elements = payjp.elements();
-
-  const numberElement = elements.create("cardNumber");
-  const expiryElement = elements.create("cardExpiry");
-  const cvcElement = elements.create("cardCvc");
-
-  numberElement.mount("#number-form");
-  expiryElement.mount("#expiry-form");
-  cvcElement.mount("#cvc-form");
-
-  const form = document.getElementById("charge-form");
-  if (!form) return;
-
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    payjp.createToken(numberElement).then((response) => {
-      if (response.error) {
-        alert("カード情報が正しくありません。");
-      } else {
-        const token = response.id;
-
-        const hiddenToken = document.createElement("input");
-        hiddenToken.setAttribute("type", "hidden");
-        hiddenToken.setAttribute("name", "token");
-        hiddenToken.setAttribute("value", token);
-        form.appendChild(hiddenToken);
-
-        numberElement.clear();
-        expiryElement.clear();
-        cvcElement.clear();
-        form.submit();
-      }
-    });
-  });
-};
-
-
-window.addEventListener("turbo:load", () => {price();pay();});
-window.addEventListener("turbo:render", () => {price();pay();});
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
